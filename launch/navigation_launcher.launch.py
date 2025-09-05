49388c1e87ad9d54f1209dae7e77bb57a92df83b
Clemente Donoso Krauss <clemente.donosok@gmail.com>
2025-09-05 15:03:22 +0200

    Add cheats to know how to use the robot

--- launch/navigation_launcher.launch.py
+++ launch/navigation_launcher.launch.py
@@ -13,7 +13,7 @@ def generate_launch_description():
                     'use_robot': False,
                     'arm_controlled': 'both',
                     'enable_arm_ui': True,
-                    'ik_use_waist': True,
+                    'ik_use_waist': False,
                     'ik_alpha': 0.2,
                     'ik_max_dq_step': 0.05,
                     'arm_velocity_limit': 2.0,
